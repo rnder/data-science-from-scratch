007d16e648165ba889bb40afd20fcac6bd3638b0
Lucy Park <me@lucypark.kr>
2015-07-05 22:51:48 +0900

    8장 코드 오류 고치고 PEP8 지키기

--- code/ch08_gradient_descent.py
+++ code/ch08_gradient_descent.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
 from __future__ import division
 from collections import Counter
-from linear_algebra import distance, vector_subtract, scalar_multiply
 import math, random
+
+from ch04_linear_algebra import distance, vector_subtract, scalar_multiply
+
 
 def sum_of_squares(v):
     """computes the sum of squared elements in v"""
     return sum(v_i ** 2 for v_i in v)
 
+
 def difference_quotient(f, x, h):
     return (f(x + h) - f(x)) / h
 
-def plot_estimated_derivative(plt):
+
+def plot_estimated_derivative():
 
     def square(x):
         return x * x
@@ -18,34 +25,40 @@ def plot_estimated_derivative(plt):
     def derivative(x):
         return 2 * x
 
-    derivative_estimate = partial(difference_quotient, square, h=0.00001)
+    derivative_estimate = lambda x: difference_quotient(square, x, h=0.00001)
 
-    # plot to show they're basically the same
-    import matplotlib.pyplot as plt
-    x = range(-10,10)
-    plt.plot(x, map(derivative, x), 'rx')           # red  x
-    plt.plot(x, map(derivative_estimate, x), 'b+')  # blue +
-    plt.show()                                      # purple *, hopefully
+    # 두 계산식에 따른 결과값이 거의 비슷함을 보여주기 위한 그래프
+    x = range(-10, 10)
+    plt.title("실제 미분값 vs. 근사값")
+    plt.plot(x, map(derivative, x), 'rx', label='실제값')           # 빨간색 x
+    plt.plot(x, map(derivative_estimate, x), 'b+', label='근사값')  # 파란색 +
+    plt.legend(loc=9)                                               # 상단 중앙 범례
+    plt.show()                                                      # 보라색 *가 나와주길!
+
 
 def partial_difference_quotient(f, v, i, h):
 
     # add h to just the i-th element of v
     w = [v_j + (h if j == i else 0)
          for j, v_j in enumerate(v)]
-         
+
     return (f(w) - f(v)) / h
+
 
 def estimate_gradient(f, v, h=0.00001):
     return [partial_difference_quotient(f, v, i, h)
-            for i, _ in enumerate(v)] 
+            for i, _ in enumerate(v)]
+
 
 def step(v, direction, step_size):
     """move step_size in the direction from v"""
     return [v_i + step_size * direction_i
             for v_i, direction_i in zip(v, direction)]
 
-def sum_of_squares_gradient(v): 
+
+def sum_of_squares_gradient(v):
     return [2 * v_i for v_i in v]
+
 
 def safe(f):
     """define a new function that wraps f and return it"""
@@ -58,47 +71,50 @@ def safe(f):
 
 
 #
-# 
+#
 # minimize / maximize batch
 #
 #
 
 def minimize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
     """use gradient descent to find theta that minimizes target function"""
-    
+
     step_sizes = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
-    
+
     theta = theta_0                           # set theta to initial value
     target_fn = safe(target_fn)               # safe version of target_fn
     value = target_fn(theta)                  # value we're minimizing
-    
+
     while True:
-        gradient = gradient_fn(theta)  
+        gradient = gradient_fn(theta)
         next_thetas = [step(theta, gradient, -step_size)
                        for step_size in step_sizes]
-                   
-        # choose the one that minimizes the error function        
+
+        # choose the one that minimizes the error function
         next_theta = min(next_thetas, key=target_fn)
         next_value = target_fn(next_theta)
-        
+
         # stop if we're "converging"
         if abs(value - next_value) < tolerance:
             return theta
         else:
             theta, value = next_theta, next_value
 
+
 def negate(f):
     """return a function that for any input x returns -f(x)"""
     return lambda *args, **kwargs: -f(*args, **kwargs)
-    
+
+
 def negate_all(f):
     """the same when f returns a list of numbers"""
     return lambda *args, **kwargs: [-y for y in f(*args, **kwargs)]
 
+
 def maximize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
     return minimize_batch(negate(target_fn),
                           negate_all(gradient_fn),
-                          theta_0, 
+                          theta_0,
                           tolerance)
 
 #
@@ -112,6 +128,7 @@ def in_random_order(data):
     for i in indexes:                          # return the data in that order
         yield data[i]
 
+
 def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
 
     data = zip(x, y)
@@ -119,7 +136,7 @@ def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
     alpha = alpha_0                             # initial step size
     min_theta, min_value = None, float("inf")   # the minimum so far
     iterations_with_no_improvement = 0
-    
+
     # if we ever go 100 iterations with no improvement, stop
     while iterations_with_no_improvement < 100:
         value = sum( target_fn(x_i, y_i, theta) for x_i, y_i in data )
@@ -135,24 +152,24 @@ def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
             iterations_with_no_improvement += 1
             alpha *= 0.9
 
-        # and take a gradient step for each of the data points        
+        # and take a gradient step for each of the data points
         for x_i, y_i in in_random_order(data):
             gradient_i = gradient_fn(x_i, y_i, theta)
             theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
-            
+
     return min_theta
+
 
 def maximize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
     return minimize_stochastic(negate(target_fn),
                                negate_all(gradient_fn),
                                x, y, theta_0, alpha_0)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     print "using the gradient"
 
     v = [random.randint(-10,10) for i in range(3)]
-
     tolerance = 0.0000001
 
     while True:
@@ -166,7 +183,6 @@ if __name__ == "__main__":
     print "minimum v", v
     print "minimum value", sum_of_squares(v)
     print
-
 
     print "using minimize_batch"
 
