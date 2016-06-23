009d426be11fa3d21546c242696a563716c1890f
Sungchul Choi <sc82.choi@gachon.ac.kr>
2016-06-23 21:36:33 +0900

    - Update ch09 ipynb files

--- code/ch06_probability.py
+++ code/ch06_probability.py
@@ -21,8 +21,7 @@ def normal_pdf(x, mu=0, sigma=1):
 def plot_normal_pdfs(plt):
     xs = [x / 10.0 for x in range(-50, 50)]
     plt.plot(xs,[normal_pdf(x,sigma=1) for x in xs],'-',label='mu=0,sigma=1')
-    plt.plot(xs,[normal_pdf(x,sigma=2) for x in xs],'--',label='mu=0,sigma=2')
-    plt.plot(xs,[normal_pdf(x,sigma=0.5) for x in xs],':',label='mu=0,sigma=0.5')
+    plt.plot(xs,[normal_pdf(x,sigåma=0.5) for x in xs],':',label='mu=0,sigma=0.5')
     plt.plot(xs,[normal_pdf(x,mu=-1)   for x in xs],'-.',label='mu=-1,sigma=1')
     plt.legend()
     plt.show()      
